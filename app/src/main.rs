114e5f262b628de7f35e06933752ef1f45976a6c
Piotr Zaczkowski <pzaczkowski@protonmail.com>
2020-10-02 16:13:41 +0200

    first working version

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,7 +1,6 @@
 //! Render example where each glyph pixel is output as an ascii character.
 use rusttype::{point, Font, Scale};
 use ssd1305::Ssd1305;
-use std::io::Write;
 
 fn main() {
     let screen = Ssd1305::new();
@@ -11,18 +10,6 @@ fn main() {
     }
     let mut screen = screen.unwrap();
     screen.begin();
-    let data = screen.data();
-    for (v, i) in data.iter_mut().enumerate() {
-        *i = (v % 256) as u8;
-    }
-    screen.display();
-    std::thread::sleep(std::time::Duration::from_millis(1000));
-    screen.begin();
-    let data = screen.data();
-    for (v, i) in data.iter_mut().rev().enumerate() {
-        *i = (v % 256) as u8;
-    }
-    screen.display();
 
     let font = if let Some(font_path) = std::env::args().nth(1) {
         let font_path = std::env::current_dir().unwrap().join(font_path);
@@ -36,17 +23,17 @@ fn main() {
     } else {
         const FONT_PATH: &str = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf";
         eprintln!("No font specified ... using {}", FONT_PATH);
-        let font_data = include_bytes!("/home/zaczkows/.fonts/SourceCodePro-Medium.otf");
+        let font_data = include_bytes!("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf");
         Font::try_from_bytes(font_data as &[u8]).expect("error constructing a Font from bytes")
     };
 
     // Desired font pixel height
-    let height: f32 = 16.4; // to get 80 chars across (fits most terminals); adjust as desired
+    let height: f32 = 18.0; // to get 80 chars across (fits most terminals); adjust as desired
     let pixel_height = height.ceil() as usize;
 
     // 2x scale in x direction to counter the aspect ratio of monospace characters.
     let scale = Scale {
-        x: height * 3.0,
+        x: height * 1.0,
         y: height,
     };
 
@@ -58,50 +45,40 @@ fn main() {
     let v_metrics = font.v_metrics(scale);
     let offset = point(0.0, v_metrics.ascent);
 
-    // Glyphs to draw for "RustType". Feel free to try other strings.
-    let glyphs: Vec<_> = font.layout("RustType", scale, offset).collect();
+    loop {
+        let now = time::OffsetDateTime::now_local();
+        // Glyphs to draw for "RustType". Feel free to try other strings.
+        let glyphs: Vec<_> = font.layout(&now.format("%T"), scale, offset).collect();
 
-    // Find the most visually pleasing width to display
-    let width = glyphs
-        .iter()
-        .rev()
-        .map(|g| g.position().x as f32 + g.unpositioned().h_metrics().advance_width)
-        .next()
-        .unwrap_or(0.0)
-        .ceil() as usize;
+        // Find the most visually pleasing width to display
+        let width = glyphs
+            .iter()
+            .rev()
+            .map(|g| g.position().x as f32 + g.unpositioned().h_metrics().advance_width)
+            .next()
+            .unwrap_or(0.0)
+            .ceil() as usize;
 
-    println!("width: {}, height: {}", width, pixel_height);
+        print!("width: {}, height: {}\r", width, pixel_height);
 
-    // Rasterise directly into ASCII art.
-    let mut pixel_data = vec![b'*'; width * pixel_height];
-    let mapping = b"*%#x+=:-. "; // The approximation of greyscale
-    let mapping_scale = (mapping.len() - 1) as f32;
-    for g in glyphs {
-        if let Some(bb) = g.pixel_bounding_box() {
-            g.draw(|x, y, v| {
-                // v should be in the range 0.0 to 1.0
-                let i = (v * mapping_scale + 0.5) as usize;
-                // so something's wrong if you get $ in the output.
-                let c = mapping.get(i).cloned().unwrap_or(b'$');
-                let x = x as i32 + bb.min.x;
-                let y = y as i32 + bb.min.y;
-                // There's still a possibility that the glyph clips the boundaries of the bitmap
-                if x >= 0 && x < width as i32 && y >= 0 && y < pixel_height as i32 {
-                    let x = x as usize;
-                    let y = y as usize;
-                    pixel_data[(x + y * width)] = c;
-                }
-            })
+        let w = screen.width() as i32;
+        let h = screen.height() as i32;
+        let data = screen.data();
+        for g in glyphs {
+            if let Some(bb) = g.pixel_bounding_box() {
+                g.draw(|x, y, v| {
+                    let x = x as i32 + bb.min.x;
+                    let y = y as i32 + bb.min.y;
+                    if x >= w || y >= h {
+                        return;
+                    }
+                    // v should be in the range 0.0 to 1.0
+                    let i = if v > 0.43 { 1 } else { 0 };
+                    data[(x + (y / 8) * w) as usize] |= i << (y % 8);
+                })
+            }
         }
-    }
-
-    // Print it out
-    let stdout = ::std::io::stdout();
-    let mut handle = stdout.lock();
-    for j in 0..pixel_height {
-        handle
-            .write_all(&pixel_data[j * width..(j + 1) * width])
-            .unwrap();
-        handle.write_all(b"\n").unwrap();
+        screen.display();
+        std::thread::sleep(std::time::Duration::from_millis(1000));
     }
 }
